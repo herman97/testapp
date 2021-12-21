e2183dbacf20a165072e0883b53bf82888b1403f
Herman <herno643@student.liu.se>
2021-12-21 19:25:09 +0100

    Initial Commit

---
+++ ak/akApp.swift
@@ -0,0 +1,17 @@
+//
+//  akApp.swift
+//  ak
+//
+//  Created by Herman Nordin on 2021-12-21.
+//
+
+import SwiftUI
+
+@main
+struct akApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
